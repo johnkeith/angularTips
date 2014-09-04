84fdd82c5deae6706359887fac4c331061cc2283
John Keith <johnkeith@outlook.com>
2014-09-04 11:50:49 -0400

    calc functioning

--- js/angular/controllers/controllers.js
+++ js/angular/controllers/controllers.js
@@ -1,10 +1,36 @@
 angular.module("controllers", [])
   .controller("tipCalcCtrl", ["$scope", "tipCalc",
     function($scope, tipCalc){
-      $scope.calculate = function(){
-        tipConverted = tipCalc.percentConvertor($scope.tip);
-        $scope.billWithTip = tipCalc.defaultCalc($scope.bill, tipConverted);
+      $scope.btnInput = function(btnVal){
+        if(btnVal == "C"){
+          $scope.clearBill();
+        }
+        else if(btnVal == "."){
+          $scope.centsPressed = true;
+        }
+        else if ($scope.centsPressed == true && $scope.bill.cents.length < 2){
+          $scope.bill.cents += btnVal;
+        }
+        else if ($scope.centsPressed == false){
+          $scope.bill.dollars += btnVal;
+        }
+        $scope.calcTotal();
       };
-      $scope.bill = 0;
-      $scope.tip = 0;
+      $scope.calcTotal = function(){
+        tipConverted = tipCalc.percentConvertor($scope.tip);
+        billConverted = tipCalc.billConvertor($scope.bill);
+        $scope.billWithTip = tipCalc.defaultCalc(billConverted, tipConverted);
+      };
+      $scope.clearBill = function(){
+        $scope.bill.dollars = "0";
+        $scope.bill.cents = "";
+        $scope.centsPressed = false;
+      };
+      $scope.bill = {
+        dollars: "0",
+        cents: ""
+      };
+      $scope.tip = "0";
+      $scope.billWithTip = "0";
+      $scope.centsPressed = false;
   }]);
